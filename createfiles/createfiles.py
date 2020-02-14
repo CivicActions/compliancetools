7d6c049feac3db20aa5c2e7b6e051fab213a817c
Tom Camp <tom.camp@civicactions.com>
2020-02-14 12:43:43 -0700

    Bug fix for createfiles.

--- createfiles/createfiles.py
+++ createfiles/createfiles.py
@@ -10,31 +10,43 @@ import click
 import secrender
 from yaml import load, FullLoader
 from yamlinclude import YamlIncludeConstructor
+from pathlib import Path
+from itertools import zip_longest, dropwhile
 
 @click.command()
 @click.option('--in', '-i', 'in_',
               required=True,
               type=click.Path(exists=True, dir_okay=False, readable=True),
               help='values (YAML)')
-@click.option('--out', '-o', 'out_',
-              type=click.Path(exists=True, dir_okay=True, readable=True),
-              default='.',
-              help='Output directory')
 @click.option('--templates', '-t', 'template_dir',
               type=click.Path(exists=True, dir_okay=True, file_okay=False),
               help='Template directory')
+@click.option('--out', '-o', 'out_',
+              type=click.Path(exists=False, dir_okay=True, readable=True),
+              default='.',
+              help='Output directory')
 def main(in_, template_dir, out_):
     template_args = load_template_args(in_)
-    all_templates = get_template_list(template_dir)
-    for template in all_templates:
-        out_file = template.replace(template_dir, '')
-        out_file = os.path.join(out_, out_file)
-        ext = os.path.splitext(out_file)
-        if ext[1] == '.j2':
-            out_file = ext[0]
-        if not os.path.exists(os.path.dirname(out_file)):
-            os.makedirs(os.path.dirname(out_file))
-        secrender.secrender(template, template_args, out_file)
+    od = Path(out_)
+    td = Path(template_dir)
+    if not od.is_dir():
+        od.mkdir(parents=True, exist_ok=True)
+
+    p = Path(td).rglob("*")
+    templates = [x for x in p if x.is_file()]
+
+    for tf in templates:
+        new_path = rewrite(tf, td, od)
+        new_file = Path(new_path)
+        if tf.name != 'component.yaml':
+            ext = new_file.suffix
+            if ext == '.j2':
+                new_file = new_file.with_name(new_file.stem)
+
+            if not new_file.parent.is_dir():
+                new_file.parent.mkdir(parents=True, exist_ok=True)
+            print("Creating file: {} from {}".format(new_file, tf))
+            secrender.secrender(tf, template_args, new_file)
 
 def load_template_args(in_):
     YamlIncludeConstructor.add_to_loader_class(loader_class=FullLoader)
@@ -42,11 +54,9 @@ def load_template_args(in_):
         yaml = load(stream, Loader=FullLoader)
     return secrender.get_template_args(yaml, None, dict())
 
-def get_template_list(template_dir):
-    file_list = list()
-    for (dirpath, dirname, filenames) in os.walk(template_dir):
-        file_list += [os.path.join(dirpath, file) for file in filenames]
-    return file_list
+def rewrite(tf, td, od):
+    subpath = [p[0] for p in dropwhile(lambda f: f[0] == f[1], zip_longest(tf.parts, td.parts))]
+    return str(od / Path(*subpath))
 
 if __name__ == '__main__':
     main()
