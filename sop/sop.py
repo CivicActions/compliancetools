eb467bd3d4ac11f4df344858db10d510ed255478
Tom Camp <tom.camp@civicactions.com>
2023-03-23 14:43:51 -0400

    Adding TOC

--- sop/sop.py
+++ sop/sop.py
@@ -4,21 +4,61 @@ from pathlib import Path
 
 import click
 import yaml  # type: ignore
+from sop_writer import SopWriter
 
 # Copyright 2019-2020 CivicActions, Inc. See the README file at the top-level
 # directory of this distribution and at https://github.com/CivicActions/compliancetools#copyright.
 #
 
 
+def aggregate_control_data(component_dir: Path) -> dict:
+    families: dict = {}
+    components = component_dir.rglob("*")
+    templates = [
+        cfile
+        for cfile in components
+        if cfile.is_file() and cfile.name != "component.yaml"
+    ]
+    for template in templates:
+        family = template.stem.lower().replace("_", "-")
+        if family not in families:
+            families[family] = {}
+
+        with open(template, "r") as tmpy:
+            component = yaml.load(tmpy, Loader=yaml.FullLoader)
+        satisfies = component.get("satisfies")
+        for control in satisfies:
+            control_key = control.get("control_key")
+            control_name = control.get("control_name").title()
+            key = f"{control_key} {control_name}"
+            if key and key not in families[family]:
+                families[family][key] = {}
+
+            for parts in control.get("narrative"):
+                part = parts.get("key", "text")
+
+                if part not in families[family][key]:
+                    families[family][key][part] = []
+                families[family][key][part].append(parts.get("text"))
+
+    sorted_families = sort_parts(families)
+    return sorted_families
+
+
+def write_files(families: dict, out_dir: Path):
+    for family, controls in families.items():
+        family_name = family[3:].replace("-", " ").title()
+        filename = out_dir.joinpath(f"sop-{family}").with_suffix(".md")
+        title = f"{family_name} ({family[:2].upper()}) Standard (SOP)"
+        text = SopWriter(
+            filepath=filename,
+            controls=controls,
+            title=title,
+        )
+        text.create_file()
+
+
 @click.command()
-@click.option(
-    "--in",
-    "-i",
-    "input_file",
-    required=True,
-    type=click.Path(exists=True, dir_okay=False, readable=True),
-    help="Replacement data values (YAML)",
-)
 @click.option(
     "--components",
     "-c",
@@ -35,41 +75,16 @@ import yaml  # type: ignore
     default=".",
     help="Output directory (default: current directory)",
 )
-def main(input_file: Path, components_dir: Path, output_dir: Path):
-    out_dir = Path(output_dir)
+def main(components_dir: Path, output_dir: Path):
+    out_dir = Path(output_dir).joinpath("sop")
     if not out_dir.is_dir():
         out_dir.mkdir(parents=True, exist_ok=True)
+
     rendered_components = Path(components_dir)
-    if rendered_components.resolve():
-        families: dict = {}
-        components = rendered_components.rglob("*")
-        templates = [
-            cfile
-            for cfile in components
-            if cfile.is_file() and cfile.name != "component.yaml"
-        ]
-        for template in templates:
-            family = template.stem.lower().replace("_", "-")
-            if family not in families:
-                families[family] = {}
 
-            with open(template, "r") as tmpy:
-                component = yaml.load(tmpy, Loader=yaml.FullLoader)
-            satisfies = component.get("satisfies")
-            for control in satisfies:
-                key = control.get("control_key")
-                if key and key not in families[family]:
-                    families[family][key] = {}
+    families = aggregate_control_data(rendered_components)
 
-                for parts in control.get("narrative"):
-                    part = parts.get("key", "text")
-
-                    if part not in families[family][key]:
-                        families[family][key][part] = []
-                    families[family][key][part].append(parts.get("text"))
-
-    sort_parts(families)
-    print(families)
+    write_files(families, out_dir)
 
 
 def sort_parts(families: dict) -> dict:
